4a7823dafc063af628f90c562be5ce6ac2052c8c
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2013-11-06 01:19:55 +0400

    ARM: sa1100: collie: don't pass irq_base to locomo
    It is unnecessary to pass irq_base to locomo, it will alloc irqs on it's own.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- arch/arm/mach-sa1100/collie.c
+++ arch/arm/mach-sa1100/collie.c
@@ -191,7 +191,6 @@ static struct resource locomo_resources[] = {
 };
 
 static struct locomo_platform_data locomo_info = {
-	.irq_base	= IRQ_BOARD_START,
 };
 
 struct platform_device collie_locomo_device = {
